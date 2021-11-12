1f22b4f5a5a9c17251ae16a7bafb9a655268bd99
AL3X-69 <26258257+AL3X-69@users.noreply.github.com>
2021-11-12 10:25:35 +0100

    Initial commit / Release of v1.0.0

---
+++ src/main/java/fr/alex6/discord/cmx/Command.java
@@ -0,0 +1,27 @@
+package fr.alex6.discord.cmx;
+
+import fr.alex6.discord.cmx.CommandModule;
+import fr.alex6.discord.cmx.ExecutionRule;
+import net.dv8tion.jda.api.Permission;
+
+import java.lang.annotation.*;
+
+/**
+ * A method annotation to register a command inside a {@link CommandModule} class, this annotation takes the following arguments
+ * - value: command name
+ * - aliases: command aliases default {}
+ * - executionRule: define if a command can be executed in private and/or text channel default {@link ExecutionRule}.TEXT_CHANNEL
+ *
+ * @see Annotation
+ * @author Alex6
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+@Documented
+public @interface Command {
+    String value();
+    String[] aliases() default {};
+    ExecutionRule executionRule() default ExecutionRule.TEXT_CHANNEL;
+    Permission[] permissions() default {};
+    boolean replyToBot() default false;
+}
