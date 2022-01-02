60892d5ae089c29c661b63e2dc6b7b40554197fa
AL3X-69 <26258257+AL3X-69@users.noreply.github.com>
2022-01-02 11:31:51 +0100

    Moved TestModule into the examples folder

--- examples/fr/alex6/discord/cmx/examples/Main.java
+++ examples/fr/alex6/discord/cmx/examples/Main.java
@@ -1,7 +1,6 @@
 package fr.alex6.discord.cmx.examples;
 
 import fr.alex6.discord.cmx.CommandManager;
-import fr.alex6.discord.cmx.impl.TestModule;
 import net.dv8tion.jda.api.JDABuilder;
 
 import javax.security.auth.login.LoginException;
--- examples/fr/alex6/discord/cmx/examples/TestModule.java
+++ examples/fr/alex6/discord/cmx/examples/TestModule.java
@@ -1,8 +1,10 @@
-package fr.alex6.discord.cmx.impl;
+package fr.alex6.discord.cmx.examples;
 
 import fr.alex6.discord.cmx.Command;
 import fr.alex6.discord.cmx.CommandModule;
+import fr.alex6.discord.cmx.hook.Event;
 import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 
 import java.util.Arrays;
 
@@ -10,5 +12,10 @@ public class TestModule extends CommandModule {
     @Command(value = "test", aliases = "t")
     public void test(Message message, String arg, String[] args) {
         message.reply("Ok ! You sent: "+arg+" | "+ Arrays.toString(args)).queue();
+    }
+
+    @Event
+    public void onMessage(MessageReceivedEvent event) {
+        System.out.println("I received the following message: "+event.getMessage().getContentRaw());
     }
 }
